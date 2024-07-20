06bf6fd1cae7def05749b9150c34968f0b1e3d65
dcunhamaciel <dcunhamaciel@gmail.com>
2024-07-20 11:58:35 -0300

    Propriedade casts para utilização dos Enums de Flow e Transport

--- app/Models/Comex.php
+++ app/Models/Comex.php
@@ -5,12 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Enums\FlowEnum;
+use App\Enums\TransportEnum;
 
 class Comex extends Model
 {
     use HasFactory;
 
     protected $table = 'comex';
+
+    protected $casts = [
+        'flow' => FlowEnum::class,
+        'transport' => TransportEnum::class,
+    ];
 
     public function country(): BelongsTo
     {
